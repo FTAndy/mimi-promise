61556df3f6466f7287841246299bcd6a1f9f069a
FTAndy <fortheandy@gmail.com>
2016-10-30 15:34:31 +0800

    complete and add readme

---
+++ mimi-promise.js
@@ -0,0 +1,141 @@
+const asyncFn = function() {
+  if (typeof process === 'object' && process !== null && typeof(process.nextTick) === 'function')
+    return process.nextTick
+  if (typeof(setImmediate === 'function'))
+    return setImmediate
+  return setTimeout
+}()
+
+const PENDING = 'PENDING'
+
+const RESOLVED = 'RESOLVED'
+
+const REJECTED = 'REJECTED'
+
+function MimiPromise(executor) {
+  this.state = PENDING
+  this.executedData = undefined
+  this.multiPromise2 = []
+
+  resolve = (value) => {
+    settlePromise(this, RESOLVED, value)
+  }
+
+  reject = (reason) => {
+    settlePromise(this, REJECTED, reason)
+  }
+
+  executor(resolve, reject)
+}
+
+MimiPromise.prototype.then = function(resolvedCallback, rejectedCallback) {
+  let promise2 = new MimiPromise(() => {})
+
+  if (typeof resolvedCallback === "function") {
+      promise2.resolvedCallback = resolvedCallback;
+  }
+  if (typeof rejectedCallback === "function") {
+      promise2.rejectedCallback = rejectedCallback;
+  }
+
+  if (this.state === PENDING) {
+    this.multiPromise2.push(promise2)
+  } else if (this.state === RESOLVED) {
+    processCallback(this, promise2, promise2.resolvedCallback)
+  } else if (this.state === REJECTED) {
+    processCallback(this, promise2, promise2.rejectedCallback)
+  }
+
+  return promise2
+}
+
+function settlePromise(promise, executedState, executedData) {
+  if (promise.state !== PENDING)
+    return
+
+  promise.state = executedState
+  promise.executedData = executedData
+
+  const callbackType = executedState === RESOLVED ? "resolvedCallback" : "rejectedCallback"
+
+  for (promise2 of promise.multiPromise2) {
+    processCallback(promise, promise2, promise2[callbackType])
+  }
+}
+
+function processCallback(promise, promise2, callback) {
+  asyncFn(() => {
+    if (!callback) {
+      settlePromise(promise2, promise.state, promise.executedData);
+      return;
+    }
+
+    let x
+
+    try {
+      x = callback(promise.executedData)
+    } catch (e) {
+      settlePromise(promise2, REJECTED, e)
+      return
+    }
+
+    settleWithX(promise2, x)
+  })
+}
+
+function settleWithX (p, x) {
+    if (x === p && x) {
+        settlePromise(p, REJECTED, new TypeError("promise_circular_chain"));
+        return;
+    }
+
+    var xthen, type = typeof x;
+    if (x !== null && (type === "function" || type === "object")) {
+        try {
+            xthen = x.then;
+        } catch (err) {
+            settlePromise(p, REJECTED, err);
+            return;
+        }
+        if (typeof xthen === "function") {
+            settleXthen(p, x, xthen);
+        } else {
+            settlePromise(p, RESOLVED, x);
+        }
+    } else {
+        settlePromise(p, RESOLVED, x);
+    }
+    return p;
+}
+
+function settleXthen (p, x, xthen) {
+    try {
+        xthen.call(x, function (y) {
+            if (!x) return;
+            x = null;
+
+            settleWithX(p, y);
+        }, function (r) {
+            if (!x) return;
+            x = null;
+
+            settlePromise(p, REJECTED, r);
+        });
+    } catch (err) {
+        if (x) {
+            settlePromise(p, REJECTED, err);
+            x = null;
+        }
+    }
+}
+
+MimiPromise.deferred = MimiPromise.defer = function() {
+    var dfd = {}
+    dfd.promise = new MimiPromise(function(resolve, reject) {
+      dfd.resolve = resolve
+      dfd.reject = reject
+    })
+    return dfd
+}
+
+module.exports = MimiPromise;
--- readme.md
+++ readme.md
@@ -1,5 +1,17 @@
-just a Promises/A+ spec implementation Project
-### Test
-`npm install -g promises-aplus-test `
+### Overview
 
-`promises-aplus-test main.js`
+Just a Promises/A+ spec implementation Project. Readable, simple, but not useful to production.
+
+
+### Why I name mimi
+
+In Chinese, mimi means breast. Since when I write code, I always think about mimi to let me focus. So I name it to express my thought to mimi. May god bless mimi.
+
+### Test
+`npm install -g promises-aplus-tests `
+
+`promises-aplus-tests mimi-promise.js`
+
+
+### License
+MIT
